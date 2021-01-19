c40a3ffee204a5252e590ae0c7d75f4df3febf91
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-01-19 21:56:49 +0100

    Add routes for /minions

--- server/api.js
+++ server/api.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const apiRouter = express.Router();
+const {
+  getAllMinions,
+  getOneMinion,
+  postMinion,
+  updateMinion,
+  deleteMinion,
+} = require('./minionsController');
+const ideasController = require('./ideasController');
+const meetingsController = require('./meetingsController');
 
-
+apiRouter.route('/minions').get(getAllMinions).post(postMinion);
+apiRouter
+  .route('/minions/:_minionId')
+  .get(getOneMinion)
+  .put(updateMinion)
+  .delete(deleteMinion);
+// apiRouter
+//   .route('/ideas')
+//   .get(ideasController.getAllIdeas)
+//   .post(ideasController.postIdea);
+// apiRouter
+//   .route('/ideas/:_ideaId')
+//   .get(ideasController.getOneIdea)
+//   .put(ideasController.updateIdea)
+//   .delete(ideasController.deleteIdea);
+// apiRouter
+//   .route('/meetings')
+//   .get(meetingsController.getAllMeetings)
+//   .post(meetingsController.postMeeting)
+//   .delete(meetingsController.deleteMeeting);
 
 module.exports = apiRouter;
